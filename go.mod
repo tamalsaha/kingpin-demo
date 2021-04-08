794f73fdd21d9e614b702ce08e5ddc2c49b63e95
Tamal Saha <tamal@appscode.com>
2021-04-08 10:59:20 -0700

    added-all

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/tamalsaha/kingpin-demo
 go 1.16
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
-	gopkg.in/alecthomas/kingpin.v1 v1.3.7
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
--- main.go
+++ main.go
@@ -1,37 +1,19 @@
 package main
 
 import (
-	"os"
-	"strings"
-	"gopkg.in/alecthomas/kingpin.v1"
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	app      = kingpin.New("chat", "A command-line chat application.")
-	debug    = app.Flag("debug", "Enable debug mode.").Bool()
-	serverIP = app.Flag("server", "Server address.").Default("127.0.0.1").IP()
-
-	register     = app.Command("register", "Register a new user.")
-	registerNick = register.Arg("nick", "Nickname for user.").Required().String()
-	registerName = register.Arg("name", "Name of user.").Required().String()
-
-	post        = app.Command("post", "Post a message to a channel.")
-	postImage   = post.Flag("image", "Image to post.").File()
-	postChannel = post.Arg("channel", "Channel to post to.").Required().String()
-	postText    = post.Arg("text", "Text to post.").Strings()
+	debug   = kingpin.Flag("debug", "Enable debug mode.").Bool()
+	timeout = kingpin.Flag("timeout", "Timeout waiting for ping.").Default("5s").OverrideDefaultFromEnvar("PING_TIMEOUT").Short('t').Duration()
+	ip      = kingpin.Arg("ip", "IP address to ping.").Required().IP()
+	count   = kingpin.Arg("count", "Number of packets to send").Int()
 )
 
 func main() {
-	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
-	// Register user
-	case register.FullCommand():
-		println(*registerNick)
-
-	// Post message
-	case post.FullCommand():
-		if *postImage != nil {
-		}
-		text := strings.Join(*postText, " ")
-		println("Post:", text)
-	}
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	fmt.Printf("Would ping: %s with timeout %s and count %d\n", *ip, *timeout, *count)
 }
